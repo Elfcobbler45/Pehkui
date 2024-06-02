c2b8387b1950685c88b8c926a049e69fcd24a2e9
Virtuoel <Virtuoel@users.noreply.github.com>
2024-06-02 15:06:32 +0300

    Cleanup

--- src/main/java/virtuoel/pehkui/mixin/client/compat1192minus/compat115plus/GameRendererMixin.java
+++ src/main/java/virtuoel/pehkui/mixin/client/compat1192minus/compat115plus/GameRendererMixin.java
@@ -7,8 +7,8 @@ import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.At.Shift;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
